79012f46769c821dd9933f887d5816602a2eb040
Harsh <gohil.harsh06@gmail.com>
2025-11-22 18:57:18 +0530

    Added basic backend setup

---
+++ Backend/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+import {PORT} from './config/serverConfig.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.send("Server is working!");
+// });
+
+app.use('/ping', (req, res) => {
+    return res.json({message: 'pong'});
+});
+
+app.listen(PORT, () => {
+console.log(`server is running on port ${PORT}`);
+
+});
