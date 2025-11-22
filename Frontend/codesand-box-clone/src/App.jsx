d2890801c6feb3e4d0c3bb631624946a85558563
Harsh <gohil.harsh06@gmail.com>
2025-11-22 14:01:48 +0530

    Added basic project setup

---
+++ Frontend/codesand-box-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h2>
+      this is conel project
+    </h2>
+    </>
+  )
+}
+
+export default App
